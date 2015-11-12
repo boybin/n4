f4ad86acd78c4b7df58be1dee2570a0acc1ba722
chenxiaobinmd2012@gmail.com <chenxiaobinmd2012@gmail.com>
2015-11-12 22:12:58 +0800

    Room dashboard

--- client/src/hotel/app/models/RoomModel.js
+++ client/src/hotel/app/models/RoomModel.js
@@ -10,4 +10,15 @@ angular.module('Hotel.Common')
                 'needFix'   : '25'
               };
             }
+
+            service.AllRooms = function(){
+                return [
+                  {roomId:'1',number:'101',type:'1',status:'1',title:'房间101',s_img:'client/res/images/Single-Bed-green.png',l_img:'client/res/images/Single-Bed-green.png'},
+                  {roomId:'2',number:'201',type:'2',status:'2',title:'房间201',s_img:'client/res/images/Two-Beds-grey.png',l_img:'client/res/images/Two-Beds-grey.png'},
+                  {roomId:'3',number:'301',type:'3',status:'3',title:'房间301',s_img:'client/res/images/Three-Beds-yellow.png',l_img:'client/res/images/Three-Beds-yellow.png'},
+                  {roomId:'4',number:'302',type:'2',status:'4',title:'房间302',s_img:'client/res/images/Two-Beds-red.png',l_img:'client/res/images/Two-Beds-red.png'},
+                  {roomId:'5',number:'303',type:'4',status:'2',title:'房间303',s_img:'client/res/images/Four-Beds-grey.png',l_img:'client/res/images/Four-Beds-grey.png'},
+                  {roomId:'5',number:'304',type:'2',status:'3',title:'房间304',s_img:'client/res/images/Two-Beds-yellow.png',l_img:'client/res/images/Two-Beds-yellow.png'},        {roomId:'6',number:'204',type:'2',status:'3',title:'房间204',s_img:'client/res/images/Two-Beds-yellow.png',l_img:'client/res/images/Two-Beds-yellow.png'}
+                ];
+          };
         });
--- client/src/hotel/room/controllers/RoomController.js
+++ client/src/hotel/room/controllers/RoomController.js
@@ -2,9 +2,15 @@ angular.module('Hotel.Room')
     .controller('RoomboardCtrl',
     function (RoomModel, FLOORS, ROOM_TYPES, ROOM_STATUS) {
         var roomboard = this;
+
+        //总共的层数设置
         roomboard.floors = FLOORS;
+        //总共的房子类型
         roomboard.room_types = ROOM_TYPES;
+        //总共的房子状态
         roomboard.room_status = ROOM_STATUS;
 
-        
+        //当前的房间板块显示的数据
+        roomboard.AllRooms = RoomModel.AllRooms();
+
     });
